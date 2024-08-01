4cd51ec31aca39fb74f568511de1314e3ec37253
jbocciadev <jboccia@gmail.com>
2024-08-01 11:10:36 +0100

    Implement pinging of portals.

--- ime_dr.py
+++ ime_dr.py
@@ -1,14 +1,20 @@
 ''' Python script to check FTP connections are up for all active portals in ImE, for DR exercise '''
 
-import os
+import os, datetime
 
 from csv import DictReader
 from time import sleep
 
 
+sourceFile = 'portals_full_info.csv'
+divider = "*-" * 20
+
+
 def drExercise():
     ''' Main function for DR exercise. Checks for csv file, loads into memory and pings/ftp checks portals. '''
-    sourceFile = 'portals_full_info.csv'
+    # In future, implement output to file.
+    # timeStamp = datetime.datetime.now().strftime('%Y%m%d-%X')
+    # destfile = f'DR_exercise_{timeStamp}'
 
     # Check for csv file in directory
     if not os.path.isfile(sourceFile):
@@ -19,17 +25,11 @@ def drExercise():
         portals = loadCsv(sourceFile)
         print(f"\nPortals loaded: {len(portals)}")
         
+        for portal in portals:
+            pingPortal(portal)
+            pass # Implement ftp connection check
 
-        for i in portals:
-            # Pings and ftps go here
-            #
-            #
-            #
-            #
-            #
-            # print(i)
-            pass
-            # print(i['SYSTEM'], i['PORTAL'])
+    print(f"\n{divider}\nDR Exercise finished.\n")
 
 
 def loadCsv(sourceFile):
@@ -50,6 +50,19 @@ def loadCsv(sourceFile):
                 sleep(.025)
         # print("")
     return portals
+
+
+def pingPortal(portal):
+    # param = '-n' if os.sys.platform().lower()=='win32' else '-c'
+    param = '-c'
+    hostname = portal['HOSTNAME']
+    print(f"\n{divider}\nPortal: {portal['SYSTEM']} | {portal['PORTAL']}")
+    response = os.system(f'ping -c 1 {hostname}')
+    
+    if response == 0:
+        print(f"\nPortal {portal['SYSTEM']} | {portal['PORTAL']} is up!")
+    else:
+        print(f"\nPortal {portal['SYSTEM']} | {portal['PORTAL']} seems to be down. Please check!")
 
 
 if __name__ == '__main__':
